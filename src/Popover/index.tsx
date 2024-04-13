48c6f0b2cb86a95b95ed287e5c8b974230ed1e77
drl990114 <drl990114@gmail.com>
2024-04-13 23:29:34 +0800

    feat: popover demo

--- src/Popover/index.tsx
+++ src/Popover/index.tsx
@@ -1,37 +1,49 @@
 // @ts-nocheck
-import type { PopoverProps as AkPopoverProps, PopoverProviderProps } from '@ariakit/react'
-import { PopoverArrow, PopoverProvider, PopoverDisclosure, PopoverHeading } from '@ariakit/react'
-import { PopoverWrapper } from './PopoverWrapper'
-import { Box } from '../Box'
+import type { PopoverProps as AkPopoverProps, PopoverProviderProps } from '@ariakit/react';
+import { PopoverDisclosure, PopoverProvider } from '@ariakit/react';
+import { Box } from '../Box';
+import { PopoverArrow, PopoverHeading, PopoverWrapper } from './styles';
 
 type PopoverOptions = Pick<PopoverProviderProps, 'placement' | 'open'> &
-  Pick<AkPopoverProps, 'onClose'>
+  Pick<AkPopoverProps, 'onClose'>;
 
 interface PopoverProps extends BaseComponentProps, PopoverOptions {
-  arrow?: boolean
-  title?: string
-  customContent?: React.ReactNode
-  placement?: PopoverProviderProps['placement']
-  children?: BaseComponentProps['children']
+  arrow?: boolean;
+  title?: string;
+  customContent?: React.ReactNode;
+  toggleOnClick?: boolean;
+  placement?: PopoverProviderProps['placement'];
+  children?: BaseComponentProps['children'];
 }
 
 const Popover: React.FC<PopoverProps> = (props) => {
-  const { arrow = true, title, children, customContent, placement, ...rest } = props
+  const {
+    arrow = true,
+    title,
+    toggleOnClick = true,
+    children,
+    customContent,
+    placement,
+    ...rest
+  } = props;
 
   return (
     <PopoverProvider placement={placement}>
-      <PopoverDisclosure toggleOnClick={false} render={(p) => <Box style={{ display: 'inline-block' }} {...p} />}>
+      <PopoverDisclosure
+        toggleOnClick={toggleOnClick}
+        render={(p) => <Box style={{ display: 'inline-block' }} {...p} />}
+      >
         {children}
       </PopoverDisclosure>
       <PopoverWrapper render={<Box />} {...rest}>
         {arrow ? (
-          <PopoverArrow style={{ width: '18px', height: '18px' }} className='arrow' />
+          <PopoverArrow />
         ) : null}
-        {title ? <PopoverHeading className='heading'>{title}</PopoverHeading> : null}
+        {title ? <PopoverHeading>{title}</PopoverHeading> : null}
         {customContent}
       </PopoverWrapper>
     </PopoverProvider>
-  )
-}
+  );
+};
 
-export default Popover
+export default Popover;
