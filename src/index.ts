f4485120869b566c50571d5a6aa0586f1864b433
drl990114 <drl990114@gmail.com>
2025-03-23 19:57:01 +0800

    refactor: reorganize exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
-export { default as Button } from './Button'
-export { default as Dialog } from './Dialog'
-export { default as Input } from './Input'
-export { default as Space } from './Space'
-export { default as Popover } from './Popover'
-export { default as Tooltip } from './Tooltip'
-export { default as Menu } from './Menu'
-export { default as Image } from './Image'
+import * as Spinners from 'react-spinners';
 
-export * from './Theme'
-export * from './Loading'
-export * from './Tooltip'
-export * from './Menu'
-export * from './Toast'
-export * from './TableOfContent'
-export * from './Popover'
+import * as Icon from '@ant-design/icons';
+import * as AriaKit from '@ariakit/react';
 
-export * from 'react-spinners'
+export { default as Button } from './Button';
+export { default as Dialog } from './Dialog';
+export { default as Input } from './Input';
+export { default as Space } from './Space';
+export { default as Popover } from './Popover';
+export { default as Tooltip } from './Tooltip';
+export { default as Menu } from './Menu';
+export { default as Image } from './Image';
 
-import * as Icon from '@ant-design/icons'
-import * as AriaKit from '@ariakit/react'
+export * from './Theme';
+export * from './Loading';
+export * from './Tooltip';
+export * from './Menu';
+export * from './Toast';
+export * from './TableOfContent';
+export * from './Popover';
 
-export { AriaKit }
-export { Icon }
+export { AriaKit, Icon, Spinners };
