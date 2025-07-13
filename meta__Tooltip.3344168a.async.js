2b3e0f3e90192170005b3be179ad25b8c4036041
drl990114 <drl990114@users.noreply.github.com>
2025-07-13 07:54:27 +0000

    deploy: 5d7cf8bee3e6c8bd9d0cb57e8f5e524aed6197e2

---
+++ meta__Tooltip.3344168a.async.js
@@ -0,0 +1,8 @@
+"use strict";(self.webpackChunkzens=self.webpackChunkzens||[]).push([[210],{21456:function(s,e,n){n.r(e),n.d(e,{demos:function(){return a}});var _=n(15009),o=n.n(_),l=n(99289),m=n.n(l),r=n(67294),z=n(27307),d=n(31081),a={"tooltip-demo-basic":{component:r.memo(r.lazy(function(){return n.e(31).then(n.bind(n,69373))})),asset:{type:"BLOCK",id:"tooltip-demo-basic",refAtomIds:["Tooltip"],dependencies:{"index.tsx":{type:"FILE",value:n(6541).Z},zens:{type:"NPM",value:"0.0.45"}},entry:"index.tsx"},context:{zens:d},renderOpts:{compile:function(){var i=m()(o()().mark(function p(){var u,h=arguments;return o()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(335).then(n.bind(n,37335));case 2:return t.abrupt("return",(u=t.sent).default.apply(u,h));case 3:case"end":return t.stop()}},p)}));function E(){return i.apply(this,arguments)}return E}()}}}},27293:function(s,e,n){n.r(e),n.d(e,{texts:function(){return o}});var _=n(27307);const o=[]},6541:function(s,e){e.Z=`import { Button, Tooltip } from 'zens';
+
+export default () => (
+  <Tooltip title='\u8FD9\u662F\u4E00\u4E2A\u63D0\u793A'>
+    <Button>\u6309\u94AE</Button>
+  </Tooltip>
+);
+`}}]);
