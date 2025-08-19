e687229cde8b04bdd3ffe5a2f2486f2e801b852d
drl990114 <drl990114@users.noreply.github.com>
2025-08-19 15:40:14 +0000

    deploy: 26c0b4f3cb12808eac42f5f7210d4ebba7418712

---
+++ meta__Space.0d07d4b5.async.js
@@ -0,0 +1,108 @@
+"use strict";(self.webpackChunkzens=self.webpackChunkzens||[]).push([[122],{39438:function(i,t,n){var v;n.r(t),n.d(t,{demos:function(){return h}});var f=n(15009),o=n.n(f),y=n(99289),p=n.n(y),s=n(67294),B=n(33628),m=n(31081),h={"space-demo-basic":{component:s.memo(s.lazy(function(){return n.e(206).then(n.bind(n,89906))})),asset:{type:"BLOCK",id:"space-demo-basic",refAtomIds:["Space"],dependencies:{"index.tsx":{type:"FILE",value:n(12632).Z},zens:{type:"NPM",value:"0.1.2"}},entry:"index.tsx"},context:{zens:m},renderOpts:{compile:function(){var r=p()(o()().mark(function d(){var a,u=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,u));case 3:case"end":return e.stop()}},d)}));function l(){return r.apply(this,arguments)}return l}()}},"space-demo-vertical":{component:s.memo(s.lazy(function(){return n.e(206).then(n.bind(n,61540))})),asset:{type:"BLOCK",id:"space-demo-vertical",refAtomIds:["Space"],dependencies:{"index.tsx":{type:"FILE",value:n(27753).Z},zens:{type:"NPM",value:"0.1.2"}},entry:"index.tsx"},context:{zens:m},renderOpts:{compile:function(){var r=p()(o()().mark(function d(){var a,u=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,u));case 3:case"end":return e.stop()}},d)}));function l(){return r.apply(this,arguments)}return l}()}},"space-demo-size":{component:s.memo(s.lazy(function(){return n.e(206).then(n.bind(n,10262))})),asset:{type:"BLOCK",id:"space-demo-size",refAtomIds:["Space"],dependencies:{"index.tsx":{type:"FILE",value:n(7877).Z},react:{type:"NPM",value:"18.3.1"},zens:{type:"NPM",value:"0.1.2"}},entry:"index.tsx"},context:{react:v||(v=n.t(s,2)),zens:m},renderOpts:{compile:function(){var r=p()(o()().mark(function d(){var a,u=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,u));case 3:case"end":return e.stop()}},d)}));function l(){return r.apply(this,arguments)}return l}()}},"space-demo-align":{component:s.memo(s.lazy(function(){return n.e(206).then(n.bind(n,30653))})),asset:{type:"BLOCK",id:"space-demo-align",refAtomIds:["Space"],dependencies:{"index.tsx":{type:"FILE",value:n(79731).Z},zens:{type:"NPM",value:"0.1.2"}},entry:"index.tsx"},context:{zens:m},renderOpts:{compile:function(){var r=p()(o()().mark(function d(){var a,u=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,u));case 3:case"end":return e.stop()}},d)}));function l(){return r.apply(this,arguments)}return l}()}},"space-demo-wrap":{component:s.memo(s.lazy(function(){return n.e(206).then(n.bind(n,26463))})),asset:{type:"BLOCK",id:"space-demo-wrap",refAtomIds:["Space"],dependencies:{"index.tsx":{type:"FILE",value:n(87695).Z},zens:{type:"NPM",value:"0.1.2"}},entry:"index.tsx"},context:{zens:m},renderOpts:{compile:function(){var r=p()(o()().mark(function d(){var a,u=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,u));case 3:case"end":return e.stop()}},d)}));function l(){return r.apply(this,arguments)}return l}()}},"space-demo-split":{component:s.memo(s.lazy(function(){return n.e(206).then(n.bind(n,93990))})),asset:{type:"BLOCK",id:"space-demo-split",refAtomIds:["Space"],dependencies:{"index.tsx":{type:"FILE",value:n(52495).Z},zens:{type:"NPM",value:"0.1.2"}},entry:"index.tsx"},context:{zens:m},renderOpts:{compile:function(){var r=p()(o()().mark(function d(){var a,u=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,u));case 3:case"end":return e.stop()}},d)}));function l(){return r.apply(this,arguments)}return l}()}}}},99797:function(i,t,n){n.r(t),n.d(t,{texts:function(){return f}});var v=n(33628);const f=[{value:"\u8BBE\u7F6E\u7EC4\u4EF6\u4E4B\u95F4\u7684\u95F4\u8DDD\u3002",paraId:0,tocIndex:0}]},79731:function(i,t){t.Z=`import { Button, Space } from 'zens';
+
+export default () => (
+  <div>
+    <div style={{ marginBottom: '40px' }}>
+      <Space align="start">
+        <span>\u5BF9\u9F50\u65B9\u5F0F: start</span>
+        <Button>\u6309\u94AE</Button>
+        <div style={{ padding: '20px', background: 'rgba(150, 150, 150, 0.2)' }}>Block</div>
+      </Space>
+    </div>
+    <div style={{ marginBottom: '40px' }}>
+      <Space align="center">
+        <span>\u5BF9\u9F50\u65B9\u5F0F: center</span>
+        <Button>\u6309\u94AE</Button>
+        <div style={{ padding: '20px', background: 'rgba(150, 150, 150, 0.2)' }}>Block</div>
+      </Space>
+    </div>
+    <div style={{ marginBottom: '40px' }}>
+      <Space align="end">
+        <span>\u5BF9\u9F50\u65B9\u5F0F: end</span>
+        <Button>\u6309\u94AE</Button>
+        <div style={{ padding: '20px', background: 'rgba(150, 150, 150, 0.2)' }}>Block</div>
+      </Space>
+    </div>
+    <div>
+      <Space align="baseline">
+        <span>\u5BF9\u9F50\u65B9\u5F0F: baseline</span>
+        <Button>\u6309\u94AE</Button>
+        <div style={{ padding: '20px', background: 'rgba(150, 150, 150, 0.2)' }}>Block</div>
+      </Space>
+    </div>
+  </div>
+);`},12632:function(i,t){t.Z=`import { Button, Space } from 'zens';
+
+export default () => (
+  <Space>
+    <Button>\u6309\u94AE1</Button>
+    <Button>\u6309\u94AE2</Button>
+    <Button>\u6309\u94AE3</Button>
+  </Space>
+);`},7877:function(i,t){t.Z=`import { useState } from 'react';
+import { Button, Space, SpaceSize } from 'zens';
+
+export default () => {
+  const [size, setSize] = useState<SpaceSize>('small' as const);
+
+  return (
+    <div>
+      <div style={{ marginBottom: '16px' }}>
+        <label>
+          <input
+            type="radio"
+            value="small"
+            checked={size === 'small'}
+            onChange={() => setSize('small')}
+          /> \u5C0F
+        </label>
+        <label style={{ marginLeft: '8px' }}>
+          <input
+            type="radio"
+            value="middle"
+            checked={size === 'middle'}
+            onChange={() => setSize('middle')}
+          /> \u4E2D
+        </label>
+        <label style={{ marginLeft: '8px' }}>
+          <input
+            type="radio"
+            value="large"
+            checked={size === 'large'}
+            onChange={() => setSize('large')}
+          /> \u5927
+        </label>
+      </div>
+      <Space size={size}>
+        <Button>\u6309\u94AE1</Button>
+        <Button>\u6309\u94AE2</Button>
+        <Button>\u6309\u94AE3</Button>
+      </Space>
+    </div>
+  );
+};
+`},52495:function(i,t){t.Z=`import { Space } from 'zens';
+
+export default () => (
+  <Space split={<span>|</span>}>
+    <a href="#">\u94FE\u63A51</a>
+    <a href="#">\u94FE\u63A52</a>
+    <a href="#">\u94FE\u63A53</a>
+  </Space>
+);`},27753:function(i,t){t.Z=`import { Button, Space } from 'zens';
+
+export default () => (
+  <Space direction="vertical">
+    <Button>\u6309\u94AE1</Button>
+    <Button>\u6309\u94AE2</Button>
+    <Button>\u6309\u94AE3</Button>
+  </Space>
+);`},87695:function(i,t){t.Z=`import { Button, Space } from 'zens';
+
+export default () => (
+  <Space size={[8, 16]} wrap>
+    {Array.from({ length: 20 }, (_, index) => (
+      <Button key={index}>\u6309\u94AE {index + 1}</Button>
+    ))}
+  </Space>
+);`}}]);
