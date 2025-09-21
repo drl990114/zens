b2d0317e1893710fe05bd680e71c52081cfb5aa5
drl990114 <drl990114@users.noreply.github.com>
2025-09-21 09:15:03 +0000

    deploy: 53c10bbfadc5de88b096be9be1cefee6ffa8dcbb

---
+++ meta__Image.5f5402e8.async.js
@@ -0,0 +1,34 @@
+"use strict";(self.webpackChunkzens=self.webpackChunkzens||[]).push([[370],{51405:function(l,t,n){n.r(t),n.d(t,{demos:function(){return g}});var h=n(15009),s=n.n(h),c=n(99289),i=n.n(c),m=n(67294),v=n(77706),p=n(31081),g={"image-demo-basic":{component:m.memo(m.lazy(function(){return n.e(682).then(n.bind(n,92092))})),asset:{type:"BLOCK",id:"image-demo-basic",refAtomIds:["Image"],dependencies:{"index.tsx":{type:"FILE",value:n(13217).Z},zens:{type:"NPM",value:"0.1.8"}},entry:"index.tsx"},context:{zens:p},renderOpts:{compile:function(){var o=i()(s()().mark(function u(){var r,d=arguments;return s()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(r=e.sent).default.apply(r,d));case 3:case"end":return e.stop()}},u)}));function a(){return o.apply(this,arguments)}return a}()}},"image-demo-empty":{component:m.memo(m.lazy(function(){return n.e(682).then(n.bind(n,60048))})),asset:{type:"BLOCK",id:"image-demo-empty",refAtomIds:["Image"],dependencies:{"index.tsx":{type:"FILE",value:n(53619).Z},zens:{type:"NPM",value:"0.1.8"}},entry:"index.tsx"},context:{zens:p},renderOpts:{compile:function(){var o=i()(s()().mark(function u(){var r,d=arguments;return s()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(r=e.sent).default.apply(r,d));case 3:case"end":return e.stop()}},u)}));function a(){return o.apply(this,arguments)}return a}()}},"image-demo-error":{component:m.memo(m.lazy(function(){return n.e(682).then(n.bind(n,69700))})),asset:{type:"BLOCK",id:"image-demo-error",refAtomIds:["Image"],dependencies:{"index.tsx":{type:"FILE",value:n(33672).Z},zens:{type:"NPM",value:"0.1.8"}},entry:"index.tsx"},context:{zens:p},renderOpts:{compile:function(){var o=i()(s()().mark(function u(){var r,d=arguments;return s()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(r=e.sent).default.apply(r,d));case 3:case"end":return e.stop()}},u)}));function a(){return o.apply(this,arguments)}return a}()}}}},42131:function(l,t,n){n.r(t),n.d(t,{texts:function(){return s}});var h=n(77706);const s=[{value:"\u56FE\u7247",paraId:0,tocIndex:0}]},13217:function(l,t){t.Z=`import { Image } from 'zens';
+
+export default () => {
+  return (
+    <Image
+      width={300}
+      height={200}
+      src="https://picsum.photos/300/200"
+    />
+  );
+};
+`},53619:function(l,t){t.Z=`import { Image } from 'zens';
+
+export default () => {
+  return (
+    <Image
+      width={300}
+      height={200}
+      src=""
+    />
+  );
+};
+`},33672:function(l,t){t.Z=`import { Image } from 'zens';
+
+export default () => {
+  return (
+    <Image
+      width={300}
+      height={200}
+      src="123"
+    />
+  );
+};
+`}}]);
---
+++ meta__Toast.37bfe6d9.async.js
@@ -0,0 +1,49 @@
+"use strict";(self.webpackChunkzens=self.webpackChunkzens||[]).push([[826],{36938:function(o,t,n){n.r(t),n.d(t,{demos:function(){return d}});var a=n(15009),e=n.n(a),_=n(99289),l=n.n(_),r=n(67294),p=n(2279),m=n(31081),d={"toast-demo-basic":{component:r.memo(r.lazy(function(){return n.e(81).then(n.bind(n,84607))})),asset:{type:"BLOCK",id:"toast-demo-basic",refAtomIds:["Toast"],dependencies:{"index.tsx":{type:"FILE",value:n(29622).Z},zens:{type:"NPM",value:"0.1.8"}},entry:"index.tsx"},context:{zens:m},renderOpts:{compile:function(){var c=l()(e()().mark(function E(){var u,T=arguments;return e()().wrap(function(s){for(;;)switch(s.prev=s.next){case 0:return s.next=2,n.e(335).then(n.bind(n,37335));case 2:return s.abrupt("return",(u=s.sent).default.apply(u,T));case 3:case"end":return s.stop()}},E)}));function i(){return c.apply(this,arguments)}return i}()}}}},58254:function(o,t,n){n.r(t),n.d(t,{texts:function(){return e}});var a=n(2279);const e=[{value:"\u63D0\u793A",paraId:0,tocIndex:0}]},29622:function(o,t){t.Z=`import { Button, Space, toast } from 'zens';
+
+export default () => {
+  const normalToast = () => {
+    toast('normal toast');
+  };
+
+  const errorToast = () => {
+    toast.error('error toast');
+  };
+
+  const longtextToast = () => {
+    toast('longtext toast'.repeat(20), {
+      duration: 10000,
+    });
+  };
+
+  const successToast = () => {
+    toast.success('success toast');
+  };
+
+  const promiseToast = () => {
+    let a = 'qweq';
+    const n = toast.loading('loading');
+
+    setTimeout(() => {
+      toast.dismiss(n);
+      toast.success('success', {
+        action: {
+          label: 'action',
+          onClick: () => {
+            toast('action');
+          },
+        },
+      });
+    }, 2000);
+  };
+
+  return (
+    <Space>
+      <Button onClick={normalToast}>toast</Button>
+      <Button onClick={errorToast}>error toast</Button>
+      <Button onClick={longtextToast}>longtext</Button>
+      <Button onClick={successToast}>success toast</Button>
+      <Button onClick={promiseToast}>promise toast</Button>
+    </Space>
+  );
+};
+`}}]);
