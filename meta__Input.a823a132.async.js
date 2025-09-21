6f7a7e4cf8e4265778133e8bab71c2a89cf5a453
drl990114 <drl990114@users.noreply.github.com>
2025-09-21 09:16:43 +0000

    deploy: 9716f58334c8a0732f9dbe28e4a9d7f9c816cdb3

---
+++ meta__Input.a823a132.async.js
@@ -0,0 +1,179 @@
+"use strict";(self.webpackChunkzens=self.webpackChunkzens||[]).push([[808],{56482:function(l,a,e){e.r(a),e.d(a,{demos:function(){return x}});var i=e(15009),t=e.n(i),v=e(99289),I=e.n(v),d=e(67294),h=e(32465),m=e(31081),c=e(88095),x={"input-demo-basic":{component:d.memo(d.lazy(function(){return e.e(91).then(e.bind(e,6309))})),asset:{type:"BLOCK",id:"input-demo-basic",refAtomIds:["Input"],dependencies:{"index.tsx":{type:"FILE",value:e(84818).Z},zens:{type:"NPM",value:"0.1.9"},"styled-components":{type:"NPM",value:"6.1.13"}},entry:"index.tsx"},context:{zens:m,"styled-components":c},renderOpts:{compile:function(){var o=I()(t()().mark(function u(){var r,s=arguments;return t()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(335).then(e.bind(e,37335));case 2:return n.abrupt("return",(r=n.sent).default.apply(r,s));case 3:case"end":return n.stop()}},u)}));function p(){return o.apply(this,arguments)}return p}()}},"input-demo-size":{component:d.memo(d.lazy(function(){return e.e(91).then(e.bind(e,73637))})),asset:{type:"BLOCK",id:"input-demo-size",refAtomIds:["Input"],dependencies:{"index.tsx":{type:"FILE",value:e(40527).Z},zens:{type:"NPM",value:"0.1.9"},"styled-components":{type:"NPM",value:"6.1.13"}},entry:"index.tsx"},context:{zens:m,"styled-components":c},renderOpts:{compile:function(){var o=I()(t()().mark(function u(){var r,s=arguments;return t()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(335).then(e.bind(e,37335));case 2:return n.abrupt("return",(r=n.sent).default.apply(r,s));case 3:case"end":return n.stop()}},u)}));function p(){return o.apply(this,arguments)}return p}()}}}},54047:function(l,a,e){e.r(a),e.d(a,{texts:function(){return t}});var i=e(32465);const t=[{value:"\u8F93\u5165\u6846\u7EC4\u4EF6\uFF0C\u652F\u6301\u591A\u79CD\u5C3A\u5BF8\u548C\u72B6\u6001\u3002",paraId:0,tocIndex:0},{value:"Input \u7EC4\u4EF6\u652F\u6301\u4E09\u79CD\u5C3A\u5BF8\uFF1A",paraId:1,tocIndex:2},{value:"small",paraId:1,tocIndex:2},{value:"\u3001",paraId:1,tocIndex:2},{value:"medium",paraId:1,tocIndex:2},{value:"\uFF08\u9ED8\u8BA4\uFF09\u3001",paraId:1,tocIndex:2},{value:"large",paraId:1,tocIndex:2},{value:"\u3002",paraId:1,tocIndex:2},{value:"\u53C2\u6570",paraId:2,tocIndex:3},{value:"\u8BF4\u660E",paraId:2,tocIndex:3},{value:"\u7C7B\u578B",paraId:2,tocIndex:3},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:3},{value:"size",paraId:2,tocIndex:3},{value:"\u8F93\u5165\u6846\u5C3A\u5BF8",paraId:2,tocIndex:3},{value:"'small' | 'medium' | 'large'",paraId:2,tocIndex:3},{value:"'medium'",paraId:2,tocIndex:3},{value:"onPressEnter",paraId:2,tocIndex:3},{value:"\u6309\u4E0B\u56DE\u8F66\u952E\u65F6\u7684\u56DE\u8C03",paraId:2,tocIndex:3},{value:"(e: KeyboardEvent) => void",paraId:2,tocIndex:3},{value:"-",paraId:2,tocIndex:3},{value:"inputRef",paraId:2,tocIndex:3},{value:"\u8F93\u5165\u6846\u7684 ref",paraId:2,tocIndex:3},{value:"React.Ref<HTMLInputElement>",paraId:2,tocIndex:3},{value:"-",paraId:2,tocIndex:3},{value:"\u9664\u4E86\u4EE5\u4E0A\u5C5E\u6027\uFF0CInput \u7EC4\u4EF6\u8FD8\u652F\u6301\u6240\u6709\u539F\u751F ",paraId:3,tocIndex:3},{value:"input",paraId:3,tocIndex:3},{value:" \u5143\u7D20\u7684\u5C5E\u6027\uFF0C\u5982 ",paraId:3,tocIndex:3},{value:"placeholder",paraId:3,tocIndex:3},{value:"\u3001",paraId:3,tocIndex:3},{value:"disabled",paraId:3,tocIndex:3},{value:"\u3001",paraId:3,tocIndex:3},{value:"readOnly",paraId:3,tocIndex:3},{value:" \u7B49\u3002",paraId:3,tocIndex:3},{value:"\u6B63\u5E38\u72B6\u6001",paraId:4,tocIndex:4},{value:": \u9ED8\u8BA4\u6837\u5F0F",paraId:4,tocIndex:4},{value:"\u60AC\u505C\u72B6\u6001",paraId:4,tocIndex:4},{value:": \u9F20\u6807\u60AC\u505C\u65F6\u8FB9\u6846\u53D8\u4E3A\u4E3B\u9898\u8272",paraId:4,tocIndex:4},{value:"\u805A\u7126\u72B6\u6001",paraId:4,tocIndex:4},{value:": \u83B7\u5F97\u7126\u70B9\u65F6\u663E\u793A\u4E3B\u9898\u8272\u8FB9\u6846\u548C\u9634\u5F71",paraId:4,tocIndex:4},{value:"\u7981\u7528\u72B6\u6001",paraId:4,tocIndex:4},{value:": \u8BBE\u7F6E ",paraId:4,tocIndex:4},{value:"disabled",paraId:4,tocIndex:4},{value:" \u5C5E\u6027",paraId:4,tocIndex:4},{value:"\u53EA\u8BFB\u72B6\u6001",paraId:4,tocIndex:4},{value:": \u8BBE\u7F6E ",paraId:4,tocIndex:4},{value:"readOnly",paraId:4,tocIndex:4},{value:" \u5C5E\u6027",paraId:4,tocIndex:4},{value:"\u9519\u8BEF\u72B6\u6001",paraId:4,tocIndex:4},{value:": \u8BBE\u7F6E ",paraId:4,tocIndex:4},{value:'data-error="true"',paraId:4,tocIndex:4},{value:" \u5C5E\u6027",paraId:4,tocIndex:4}]},84818:function(l,a){a.Z=`import { Input } from 'zens';
+import styled from 'styled-components';
+
+const DemoContainer = styled.div\`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  max-width: 400px;
+\`;
+
+const SizeGroup = styled.div\`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+\`;
+
+const Label = styled.label\`
+  font-size: 14px;
+  font-weight: 500;
+  color: \${props => props.theme.primaryFontColor};
+  margin-bottom: 4px;
+\`;
+
+export default () => {
+  const handlePressEnter = () => {
+    alert('You pressed enter!');
+  };
+
+  return (
+    <DemoContainer>
+      <SizeGroup>
+        <Label>Small Size:</Label>
+        <Input 
+          size="small" 
+          placeholder="Small input" 
+          onPressEnter={handlePressEnter}
+        />
+      </SizeGroup>
+
+      <SizeGroup>
+        <Label>Medium Size (default):</Label>
+        <Input 
+          placeholder="Medium input" 
+          onPressEnter={handlePressEnter}
+        />
+      </SizeGroup>
+
+      <SizeGroup>
+        <Label>Large Size:</Label>
+        <Input 
+          size="large" 
+          placeholder="Large input" 
+          onPressEnter={handlePressEnter}
+        />
+      </SizeGroup>
+
+      <SizeGroup>
+        <Label>Disabled Input:</Label>
+        <Input 
+          placeholder="Disabled input" 
+          disabled
+        />
+      </SizeGroup>
+
+      <SizeGroup>
+        <Label>Read-only Input:</Label>
+        <Input 
+          placeholder="Read-only input" 
+          readOnly
+          defaultValue="Read-only value"
+        />
+      </SizeGroup>
+
+      <SizeGroup>
+        <Label>Error Input:</Label>
+        <Input 
+          placeholder="Error input" 
+          data-error="true"
+        />
+      </SizeGroup>
+    </DemoContainer>
+  );
+};
+`},40527:function(l,a){a.Z=`import { Input } from 'zens';
+import styled from 'styled-components';
+
+const DemoContainer = styled.div\`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  max-width: 500px;
+\`;
+
+const SizeRow = styled.div\`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+\`;
+
+const SizeLabel = styled.div\`
+  width: 80px;
+  font-size: 14px;
+  font-weight: 500;
+  color: \${props => props.theme.primaryFontColor};
+\`;
+
+const InputWrapper = styled.div\`
+  flex: 1;
+\`;
+
+export default () => {
+  const handlePressEnter = () => {
+    alert('You pressed enter!');
+  };
+
+  return (
+    <DemoContainer>
+      <h3 style={{ margin: '0 0 16px 0', color: '#333' }}>
+        Input Component Size Comparison
+      </h3>
+      
+      <SizeRow>
+        <SizeLabel>Small:</SizeLabel>
+        <InputWrapper>
+          <Input 
+            size="small" 
+            placeholder="Small input field" 
+            onPressEnter={handlePressEnter}
+          />
+        </InputWrapper>
+      </SizeRow>
+
+      <SizeRow>
+        <SizeLabel>Medium:</SizeLabel>
+        <InputWrapper>
+          <Input 
+            size="medium" 
+            placeholder="Medium input field (default)" 
+            onPressEnter={handlePressEnter}
+          />
+        </InputWrapper>
+      </SizeRow>
+
+      <SizeRow>
+        <SizeLabel>Large:</SizeLabel>
+        <InputWrapper>
+          <Input 
+            size="large" 
+            placeholder="Large input field" 
+            onPressEnter={handlePressEnter}
+          />
+        </InputWrapper>
+      </SizeRow>
+
+      <SizeRow>
+        <SizeLabel>Disabled:</SizeLabel>
+        <InputWrapper>
+          <Input 
+            size="medium" 
+            placeholder="Disabled input field" 
+            disabled
+          />
+        </InputWrapper>
+      </SizeRow>
+
+      <SizeRow>
+        <SizeLabel>Error:</SizeLabel>
+        <InputWrapper>
+          <Input 
+            size="medium" 
+            placeholder="Error input field" 
+            data-error="true"
+          />
+        </InputWrapper>
+      </SizeRow>
+    </DemoContainer>
+  );
+};
+`}}]);
---
+++ meta__Popover.0ccbf256.async.js
@@ -0,0 +1,11 @@
+"use strict";(self.webpackChunkzens=self.webpackChunkzens||[]).push([[503],{99789:function(r,n,e){e.r(n),e.d(n,{demos:function(){return a}});var _=e(15009),o=e.n(_),m=e(99289),d=e.n(m),t=e(67294),P=e(89035),l=e(31081),a={"popover-demo-basic":{component:t.memo(t.lazy(function(){return e.e(425).then(e.bind(e,39858))})),asset:{type:"BLOCK",id:"popover-demo-basic",refAtomIds:["Popover"],dependencies:{"index.tsx":{type:"FILE",value:e(473).Z},zens:{type:"NPM",value:"0.1.9"}},entry:"index.tsx"},context:{zens:l},renderOpts:{compile:function(){var i=d()(o()().mark(function E(){var u,v=arguments;return o()().wrap(function(s){for(;;)switch(s.prev=s.next){case 0:return s.next=2,e.e(335).then(e.bind(e,37335));case 2:return s.abrupt("return",(u=s.sent).default.apply(u,v));case 3:case"end":return s.stop()}},E)}));function p(){return i.apply(this,arguments)}return p}()}}}},63492:function(r,n,e){e.r(n),e.d(n,{texts:function(){return o}});var _=e(89035);const o=[]},473:function(r,n){n.Z=`import { Popover } from 'zens';
+
+export default () => {
+
+  return (
+    <Popover title='Popover' fixed>
+      actions
+    </Popover>
+  );
+};
+`}}]);
